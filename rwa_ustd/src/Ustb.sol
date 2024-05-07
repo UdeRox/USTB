f65584de27ec85a2097895da565c76064d946d23
udeshika <perera.n.u@gmail.com>
2024-05-07 21:18:51 +0800

    Update initial contract

---
+++ rwa_ustd/src/Ustb.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.25;
+/**
+ * @title Ustbc
+ * @author Udeshika Perera
+ */
+contract Ustbc {
+    ///Send an http request
+    /// 1. See how much bought or how much buy,
+    /// 2. if enough have mind USTBC
+    /// 2 Transactions in singel function
+    function sendBuyRequest() external {}
+
+    function _mintFulfillRequest() internal {}
+
+    /// @notice User sends request to sell
+    /// Buy USDC on external broker protal
+    /// Send USDC to this contract for the uer to withdraw
+    function sendSellRequest() external {}
+
+    function _setllFullFillRequest() internal {}
+}
