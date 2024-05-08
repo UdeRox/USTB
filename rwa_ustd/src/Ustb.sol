2d4e882a2e46c8618029a313adc9c73b466154fc
udeshika <perera.n.u@gmail.com>
2024-05-08 18:46:06 +0800

    updated libs

--- rwa_ustd/src/Ustb.sol
+++ rwa_ustd/src/Ustb.sol
@@ -3,19 +3,23 @@
 pragma solidity 0.8.25;
 
 import {ConfirmedOwner} from "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
+import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
+import {FunctionsClient} from "@chainlink/contracts/src/v0.8/functions/dev/v1_0_0/FunctionsClient.sol";
 
 /**
  * @title Ustbc
  * @author Udeshika Perera
  */
-contract Ustbc is ConfirmedOwner {
-    constructor() ConfirmedOwner(msg.sender) {}
+contract Ustbc is ConfirmedOwner, FunctionsClient, Pausable {
+    constructor(
+        address functionsRouter
+    ) ConfirmedOwner(msg.sender) FunctionsClient(functionsRouter) {}
 
     ///Send an http request
     /// 1. See how much bought or how much buy,
     /// 2. if enough have mind USTBC
     /// 2 Transactions in singel function
-    function sendBuyRequest() external {}
+    function sendBuyRequest() external onlyOwner whenNotPaused {}
 
     function _mintFulfillRequest() internal {}
 
@@ -25,4 +29,17 @@ contract Ustbc is ConfirmedOwner {
     function sendSellRequest() external {}
 
     function _setllFullFillRequest() internal {}
+
+    // constructor(
+    //     address newOwner
+    // )
+    //     override(ConfirmedOwner, FunctionsClient)
+    //     ConfirmedOwnerWithProposal(newOwner, address(0))
+    // {}
+
+    function fulfillRequest(
+        bytes32 requestId,
+        bytes memory response,
+        bytes memory err
+    ) internal virtual override {}
 }
