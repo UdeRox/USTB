1ef4d4ad305aadc48b2f586ef9209ca8af66b556
udeshika <perera.n.u@gmail.com>
2024-05-16 19:12:01 +0800

    Mint the TBill tokens to stable coin

--- scaffold-eth-2/packages/hardhat/deploy/00_deploy_your_contract.ts
+++ scaffold-eth-2/packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -3,12 +3,12 @@ import { DeployFunction } from "hardhat-deploy/types";
 import { Contract } from "ethers";
 
 /**
- * Deploys a contract named "YourContract" using the deployer account and
+ * Deploys a contract named "tBillToken" using the deployer account and
  * constructor arguments set to the deployer address
  *
  * @param hre HardhatRuntimeEnvironment object.
  */
-const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+const tBillToken: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   /*
     On localhost, the deployer account is the one that comes with Hardhat, which is already funded.
 
@@ -22,10 +22,11 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  await deploy("YourContract", {
+  // await deploy("tBillToken", {
+  await deploy("tBillToken", {
     from: deployer,
     // Contract constructor arguments
-    args: [deployer],
+    args: ["0x779877A7B0D9E8603169DdbD7836e478b4624789"],
     log: true,
     // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
     // automatically mining the contract deployment transaction. There is no effect on live networks.
@@ -33,12 +34,14 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   });
 
   // Get the deployed contract to interact with it after deploying.
-  const yourContract = await hre.ethers.getContract<Contract>("YourContract", deployer);
-  console.log("👋 Initial greeting:", await yourContract.greeting());
+  // const tBillToken = await hre.ethers.getContract<Contract>("tBillToken", deployer);
+  const tBillToken = await hre.ethers.getContract<Contract>("tBillToken", deployer);
+  console.log("👋 Initial greeting:", await tBillToken.getAddress());
 };
 
-export default deployYourContract;
+export default tBillToken;
 
 // Tags are useful if you have multiple deploy files and only want to run one of them.
-// e.g. yarn deploy --tags YourContract
-deployYourContract.tags = ["YourContract"];
+// e.g. yarn deploy --tags tBillToken
+// tBillToken.tags = ["tBillToken"];
+tBillToken.tags = ["tBillToken"];
