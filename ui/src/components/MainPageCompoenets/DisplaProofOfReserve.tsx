a16c20089df3a2a85a5eb437e9b9948581e74dbd
1234 <arpitsinghthakur321@gmail.com>
2024-06-02 19:38:04 +0530

    updated proof reserve value on main page

---
+++ ui/src/components/MainPageCompoenets/DisplaProofOfReserve.tsx
@@ -0,0 +1,41 @@
+// src/components/DisplaProofOfReserve.tsx
+import { useEffect, useState } from "react";
+
+const DisplaProofOfReserve = () => {
+  const [maxUSDSupply, setMaxUSDSupply] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const apiResponse = await fetch("https://ustb-api-backend.vercel.app/api/portfolio/");
+        const data = await apiResponse.json();
+        setMaxUSDSupply(data.portfolioUsd);
+      } catch (err) {
+        console.error("Error fetching data:", err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const formatNumber = (value) => {
+    const formattedNumber = value.toLocaleString("en-US", {
+      minimumFractionDigits: 3,
+      maximumFractionDigits: 3,
+    });
+    const parts = formattedNumber.split(".");
+    const wholeNumber = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    const decimalPart = parts[1];
+    return `${wholeNumber}.${decimalPart}`;
+  };
+
+  return (
+    <>
+      <div className="text-3xl text-green-600 font-bold">
+       $ {maxUSDSupply ? formatNumber(maxUSDSupply) : ""}
+      </div>
+    </>
+  );
+};
+
+export default DisplaProofOfReserve;
